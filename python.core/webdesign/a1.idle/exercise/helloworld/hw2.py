850fa36066d8aa1a31b37a098ff96a9437b8af31
suma1505 <umaswaminathan15@gmail.com>
2026-03-02 20:46:38 -0800

    Delete python.core/webdesign/a1.idle/exercise directory

--- python.core/webdesign/a1.idle/exercise/helloworld/hw2.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template
-app = Flask("Hello World")
-@app.route('/', methods=['GET', 'POST'])
-def homePage():
-    return render_template("hi.html", author="cmr2su")
-app.run(debug=True, host="127.0.0.1", port=1234)
